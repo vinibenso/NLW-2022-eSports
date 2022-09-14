e6942629942095da118473e07e7e2d6c02ca199c
Vinicius Benso <vinibenso@gmail.com>
2022-09-14 19:58:02 -0300

    mobile background

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,35 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-      <Button />
-    </View>
+    <Background>
+
+      
+    </Background>
+
+    
+  
+  
   );
 }
 
-interface ButtonProps {
-  title: string
-}
-
-function Button(){
-  return(
-    <TouchableOpacity>
-      <Text>
-        Hello
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
